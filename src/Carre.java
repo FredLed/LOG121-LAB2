f7be072ef7580b23048d63be4c1ef97cc8534242
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2015-06-04 21:59:36 -0400

    Suppression de la méthode draw dans carre

--- src/Carre.java
+++ src/Carre.java
@@ -24,24 +24,4 @@ public class Carre extends Rectangle{
 		super(nseq,couleur,sommets);
 
 	}
-	
-	/* (non-Javadoc)
-	 * @see Rectangle#draw(java.awt.Graphics)
-	 */
-	public void draw(Graphics g, int position){
-		
-		final Graphics2D g2 = (Graphics2D) g.create();
-		g2.setColor(Color.BLACK);
-		
-		g.setColor(getCouleur());
-		
-		g.fillRect((position * 40),(position * 40),
-				(sommet2.x - sommet1.x), (sommet2.y - sommet1.y));
-		
-		g2.setStroke(dashed);
-		g2.drawRect((position * 40), (position * 40), (sommet2.x - sommet1.x), (sommet2.y - sommet1.y));
-		
-		g2.dispose();
-		
-	}
 }
